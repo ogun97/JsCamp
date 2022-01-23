eb2bfdc5b0876a8351f64c013ceffd7db2ca8a87
Sonsuz Dongu <sonsuz_dongu_youtube@hotmail.com>
2022-01-23 15:41:18 +0300

    23 ocak ilk ders notlarım

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 console.log("Merhaba Kodlama.io")
 
-//JS type safe değildir
+
 let dolarBugun = 9.30
 
 let dolarDun = 9.20
@@ -16,21 +16,19 @@ const euroDun = 11.2
 
 console.log(euroDun)
 
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
 
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+
+for (let i=0;i<3;i++) {
+    
+    console.log("<li>"+ konutKredileri [i]+ "</li>")
+    
 }
+    
+
 console.log("</ul>")
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
